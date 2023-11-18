39038de0aac50d05c00c5a252c9e6301d20d4a9c
Ruszeii <123785723+Ruszeii@users.noreply.github.com>
2023-11-18 17:03:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-// Define the quiz data
 const apiUrl = "https://raw.githubusercontent.com/ruszeii/Project-3/main/db.json";
 
-// Global variables
 let currentQuiz = null;
 let currentQuestionIndex = 0;
 let correctAnswers = 0;
 let answeredQuestions = 0;
 let startTime;
 
-// Function to fetch quiz data from the API
 async function fetchQuizData() {
     try {
         const response = await fetch(apiUrl);
@@ -20,7 +17,6 @@ async function fetchQuizData() {
     }
 }
 
-// Function to start the quiz
 async function startQuiz() {
     try {
         const selectedQuizValue = document.getElementById("quiz-select").value;
@@ -44,23 +40,17 @@ async function startQuiz() {
         alert('An error occurred while starting the quiz. Please try again later.');
     }
 }
-
-// Function to display the next question
 function showNextQuestion() {
     if (currentQuestionIndex < currentQuiz.length) {
         const question = currentQuiz[currentQuestionIndex];
         const questionText = question.text;
         const answerOptions = question.answerOptions;
 
-        // Update question text
         const questionElement = document.getElementById('question-text');
         questionElement.textContent = questionText;
 
-        // Clear existing answer options
         const answerOptionsElement = document.getElementById('answer-options');
         answerOptionsElement.innerHTML = '';
-
-        // Create and append answer options
         for (const answerOption of answerOptions) {
             const answerOptionElement = document.createElement('input');
             answerOptionElement.type = 'radio';
@@ -78,43 +68,34 @@ function showNextQuestion() {
     }
 }
 
-// Function to evaluate the user's answer
 function evaluateAnswer(userAnswer) {
     const currentQuestion = currentQuiz[currentQuestionIndex - 1];
     if (currentQuestion.answerOptions.find(option => option.text === userAnswer && option.isCorrect)) {
         correctAnswers++;
-        // Display correct answer message
+
         showFeedbackView('Brilliant!');
     } else {
-        // Display incorrect answer message with correct answer explanation
         const correctAnswerText = currentQuestion.answerOptions.find(option => option.isCorrect).text;
         showFeedbackView(`Incorrect! The correct answer is: ${correctAnswerText}`);
     }
 }
 
-// Function to show the feedback view
 function showFeedbackView(feedbackMessage) {
-    // Display feedback message
     const feedbackTextElement = document.getElementById('feedback-text');
     feedbackTextElement.textContent = feedbackMessage;
 
-    // Show the feedback view
     const feedbackView = document.getElementById('feedback-view');
     feedbackView.style.display = 'block';
 
-    // Hide the feedback view after 1000 milliseconds (1 second)
     setTimeout(() => {
         feedbackView.style.display = 'none';
-        showNextQuestion(); // Proceed to the next question
+        showNextQuestion(); 
     }, 1000);
 }
-
-// Function to show the completion view
 function showCompletionView() {
-    const elapsedTime = Math.floor((Date.now() - startTime) / 1000); // Elapsed time in seconds
+    const elapsedTime = Math.floor((Date.now() - startTime) / 1000); 
     const totalScore = Math.round((correctAnswers / answeredQuestions) * 100);
 
-    // Display completion message and options
     const completionMessageElement = document.getElementById('completion-message');
     if (totalScore >= 80) {
         completionMessageElement.textContent = `Congratulations ${document.getElementById('name').value}! You passed the quiz.`;
@@ -122,23 +103,19 @@ function showCompletionView() {
         completionMessageElement.textContent = `Sorry ${document.getElementById('name').value}, you failed the quiz.`;
     }
 
-    // Show the completion view
     const completionView = document.getElementById('completion-view');
     completionView.style.display = 'block';
 
-    // Update scoreboard
     document.getElementById('answered-count').textContent = answeredQuestions;
     document.getElementById('elapsed-time').textContent = elapsedTime;
     document.getElementById('total-score').textContent = totalScore;
 }
 
-// Event listener for the start quiz form submission
 document.getElementById("start-quiz-form").addEventListener("submit", function (event) {
     event.preventDefault();
     startQuiz();
 });
 
-// Event listener for submitting an answer
 document.getElementById("submit-answer").addEventListener("click", function () {
     const selectedAnswer = document.querySelector('input[name="answer"]:checked');
     if (selectedAnswer) {
@@ -146,22 +123,14 @@ document.getElementById("submit-answer").addEventListener("click", function () {
         evaluateAnswer(selectedAnswer.value);
     }
 });
-
-// Event listener for the "Got It" button in feedback view
 document.getElementById("got-it-button").addEventListener("click", function () {
-    // Logic to proceed to the next question after feedback
     showNextQuestion();
 });
 
-// Event listener for retrying the quiz
 document.getElementById("retry-quiz").addEventListener("click", function () {
-    // Reset the quiz and start over
     startQuiz();
 });
 
-// Event listener for returning to the main page
 document.getElementById("return-to-main").addEventListener("click", function () {
-    // Logic to go back to the main page
-    // For simplicity, you can reload the page
     location.reload();
 });
